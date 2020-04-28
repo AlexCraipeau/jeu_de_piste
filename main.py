6a189ad49380814bd150d39e482a8d82d24a6c8b
Alexis Craipeau <a.craipeau@gmail.com>
2020-04-28 18:53:04 +0200

    pep8 et commentaires main.py

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
 from kivy.app import App
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.image import Image
 from kivy.uix.behaviors import ButtonBehavior
-from kivy.core.window import Window
-from kivy.base import EventLoop
 from zbarcam import *
-from kivy.properties import ObjectProperty
 
-screen_manager = ScreenManager()
+# Objets globaux
+screen_manager = ScreenManager() # Gestionnaire de changement d'écran
 
 
+##################################################
+# SecondaryScreen - Tout écran différent de la map
+# Gère la touche BACK sur Android
+##################################################
 class SecondaryScreen(Screen):
-    # Gestion de la touche BACK d'Android -> retour au main screen
-
     def __init__(self, **kwargs):
         super(SecondaryScreen, self).__init__(**kwargs)
         Window.bind(on_keyboard=self.android_back_click)
 
+    # Gestion de la touche BACK d'Android
     def android_back_click(self, window, key, *largs):
-        if key == 27:
+        if key == 27: # ESC (BACK sur Android)
             screen_manager.current = 'main'
             return True
 
 
+##################################################
+# JdpGrid - Ecran principal (map)
+##################################################
 class JdpGrid(Screen):
     pass
 
 
+##################################################
+# QrcodeScreen - Ecran de récupération des qrcodes
+##################################################
 class QrcodeScreen(SecondaryScreen):
     pass
 
 
+##################################################
+# JdpMain - Point d'entrée de l'application
+##################################################
 class JdpMain(App):
     def build(self):
         screen_manager.add_widget(JdpGrid(name='main'))
