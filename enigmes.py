80ad4436f78414fcef62d98f2f2364950b7f827f
Alexis Craipeau <a.craipeau@gmail.com>
2021-05-27 11:44:50 +0200

    Ajout de l'enigme dessin (premiere version) - ajout des ressources nécessaires (amrqueurs, etc...) - ajout des textes à imprimer pour référence

--- enigmes.py
+++ enigmes.py
@@ -10,13 +10,13 @@ import yaml
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
 from kivy.lang import Builder
-import kivy_garden.mapview
+from kivy_garden.mapview import MapView
 from functools import partial
 from kivy.clock import Clock
 import zbarcam
 from plyer import gps
 
-from main import clear_sound, clear_enigme_6_lievre
+from main import clear_sound, clear_enigme_6_lievre, add_log, search_log
 
 # ##################################################
 # # LievreScreen - Ecran de l'enigme Lievre & Tortue
@@ -64,15 +64,53 @@ class LievreScreen(SecondaryScreen):
 
 class DessinScreen(SecondaryScreen):
 
+    next_location = 1
+    next_lat = 48.84208
+    next_lon = 2.388240
 # au besoin : https://stackoverflow.com/questions/40829408/how-to-trace-a-path-in-kivy-map
 # pour dessiner direct sur la carte
 # https://kivy-garden.github.io/mapview/mapview.html
 # https://www.youtube.com/watch?v=P940dd1VxsU
 
     def on_gps_location(self, **kwargs):
+
+        def get_next_loc_coordinates(self):
+            return self.ids['point_' + str(self.next_location)].lat, self.ids['point_' + str(self.next_location)].lon
+            # print(kwargs)
+
+        self.ids['player'].lon = str(kwargs['lon'])
+        self.ids['player'].lat = str(kwargs['lat'])
+
         self.ids['lat'].text = str(kwargs['lat'])
         self.ids['lon'].text = str(kwargs['lon'])
-        # print(kwargs)
+        self.ids['map'].center_on((kwargs['lat'],kwargs['lon']))
+
+        if (self.ids['centerbutton'].disabled == False
+                and self.ids['player'].lat > self.next_lat-0.00001
+                and self.ids['player'].lat < self.next_lat+ 0.00001
+                and self.ids['player'].lon > self.next_lon-0.00001
+                and self.ids['player'].lat < self.next_lon + 0.00001):
+            print(" Dessin - point trouvé !")
+            #changement de couleur point actuel (complété)
+            self.ids['point_' + str(self.next_location)].source = "./resources/images/gpsmarker.png"
+
+            if self.next_location < 7:
+                # changement du next_location
+                self.next_location = self.next_location + 1
+                # changement des next_lat/next_long
+                self.next_lat, self.next_long = get_next_loc_coordinates()
+                # affichage du point suivant
+                self.ids['point_' + str(self.next_location)].source = "./resources/images/gpsmarker_next.png"
+            elif self.location == 7:
+                add_log(App.get_running_app().root, search_log("log_12"))
+                self.next_lat = 10
+                self.next_long = 10
+                self.next_location = 8
+                #evite de charger l'image en boucle - solution de merde
+            else:
+                print("Dessin - fini")
+
+
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
